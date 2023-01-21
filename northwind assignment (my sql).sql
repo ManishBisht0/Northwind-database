371007d2e2d5e92933b950b39b3057c9f265fb47
Manish Bisht <123232512+ManishBisht0@users.noreply.github.com>
2023-01-21 14:32:15 +0530

    Add files via upload

---
+++ northwind assignment (my sql).sql
@@ -0,0 +1,75 @@
+use northwind;
+
+
+
+SELECT ProductName, QuantityPerUnit 
+FROM Products;
+
+
+SELECT ProductID, ProductName
+FROM Products;
+
+
+
+
+SELECT ProductID, ProductName
+FROM Products
+WHERE Discontinued = 1; 
+
+
+
+
+
+SELECT ProductName, UnitPrice 
+FROM Products 
+ORDER BY UnitPrice DESC;
+
+
+
+
+SELECT ProductID, ProductName, UnitPrice
+FROM Products
+WHERE (((UnitPrice)<20) AND ((Discontinued)=False))
+ORDER BY UnitPrice DESC;
+
+
+
+
+SELECT ProductName, UnitPrice
+FROM Products
+WHERE (((UnitPrice)>=15 And (UnitPrice)<=25) 
+AND ((Products.Discontinued)=False))
+ORDER BY Products.UnitPrice DESC;
+
+
+
+
+SELECT DISTINCT ProductName, UnitPrice
+FROM Products
+WHERE UnitPrice > (SELECT avg(UnitPrice) FROM Products)
+ORDER BY UnitPrice;
+
+
+
+
+
+SELECT DISTINCT ProductName as Twenty_Most_Expensive_Products, UnitPrice
+FROM Products AS a
+WHERE 20 >= (SELECT COUNT(DISTINCT UnitPrice)
+                    FROM Products AS b
+                    WHERE b.UnitPrice >= a.UnitPrice)
+ORDER BY UnitPrice desc;
+
+
+
+
+SELECT Count(ProductName)
+FROM Products
+GROUP BY Discontinued;
+
+
+
+
+SELECT ProductName,  UnitsOnOrder , UnitsInStock
+FROM Products
+WHERE (((Discontinued)=False) AND ((UnitsInStock)<UnitsOnOrder));
